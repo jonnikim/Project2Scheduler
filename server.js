d0c3abadf17c206acb69d95c216a927fea76b274
Jeff Q <Jeffquittman@gmail.com>
2020-07-09 17:51:07 -0700

    adding jeff's updated files to the repo

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const express = require("express");
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
 
 // Sets up the Express App
 // =============================================================
@@ -6,15 +8,16 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-const db = require("./models");
+const db = require('./models');
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
-  });
+	app.listen(PORT, () => {
+		console.log('App listening on PORT ' + PORT);
+	});
 });
