c579f66eec7ac4f993b7825f08a2c40a748484fe
Jonathan <jonathan213kim@gmail.com>
2020-07-19 20:49:03 -0700

    last minute polishing on routes/nav

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ app.use(passport.session());
 require("./routes")(app);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
+// app.use(function (req, res, next) {
+//   const err = new Error("Not Found");
+//   err.status = 404;
+//   next(err);
+// });
 
 // app.use(function (err, req, res, next) {
 //   res.status(err.status || 500);
