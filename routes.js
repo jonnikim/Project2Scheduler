b46d817b500383f3a326dd9d8e6ceafa48d9c4d8
Jonathan <jonathan213kim@gmail.com>
2020-08-01 16:03:45 -0700

    added AzureAD & Microsoft Graph authentication/API, as well as send email function

--- routes.js
+++ routes.js
@@ -1,9 +1,7 @@
 module.exports = function (app) {
-	const application = require('./routes/application');
-	const admin = require('./routes/admin');
-	const client = require('./routes/client');
+  const application = require("./routes/application");
+  const client = require("./routes/client");
 
-	app.use('/', application);
-	app.use('/admin', admin);
-	app.use('/client', client);
+  app.use("/", application);
+  app.use("/client", client);
 };
--- server.js
+++ server.js
@@ -1,61 +1,167 @@
 //Dependencies
+require("dotenv").config();
+const graph = require("./controllers/mail_controller");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const OIDCStrategy = require("passport-azure-ad").OIDCStrategy;
 const express = require("express");
 const exphbs = require("express-handlebars");
-const sessions = require("express-session");
 const app = express();
+const logger = require("morgan");
+const flash = require("connect-flash");
 const path = require("path");
-//const db = require('./models');
-const passport = require("passport");
-const MemoryStore = require("memorystore")(sessions);
+const authRouter = require("./routes/auth");
+const admin = require("./routes/admin");
+
+app.use(require("body-parser").urlencoded({ extended: true }));
+
+let users = {};
+
+// Passport calls serializeUser and deserializeUser to
+// manage users
+passport.serializeUser(function (user, done) {
+  // Use the OID property of the user as a key
+  users[user.profile.oid] = user;
+  done(null, user.profile.oid);
+});
+
+passport.deserializeUser(function (id, done) {
+  done(null, users[id]);
+});
+const oauth2 = require("simple-oauth2").create({
+  client: {
+    id: process.env.OAUTH_APP_ID,
+    secret: process.env.OAUTH_APP_PASSWORD,
+  },
+  auth: {
+    tokenHost: process.env.OAUTH_AUTHORITY,
+    authorizePath: process.env.OAUTH_AUTHORIZE_ENDPOINT,
+    tokenPath: process.env.OAUTH_TOKEN_ENDPOINT,
+  },
+});
+
+////////////////////////////////////////////
+// Office 365 API for organization purposes
+////////////////////////////////////////////
+async function signInComplete(
+  iss,
+  sub,
+  profile,
+  accessToken,
+  refreshToken,
+  params,
+  done
+) {
+  if (!profile.oid) {
+    return done(new Error("No OID found in user profile."));
+  }
+
+  try {
+    const user = await graph.getUserDetails(accessToken);
+    const mail = await graph.sendMail(accessToken);
+    if (user) {
+      // Add properties to profile
+      profile["email"] = user.mail ? user.mail : user.userPrincipalName;
+    }
+  } catch (err) {
+    return done(err);
+  }
+
+  // Create a simple-oauth2 token from raw tokens
+  let oauthToken = oauth2.accessToken.create(params);
+
+  // Save the profile and tokens in user storage
+  users[profile.oid] = { profile, oauthToken };
+  return done(null, users[profile.oid]);
+}
+
+// Configure OIDC strategy
+passport.use(
+  new OIDCStrategy(
+    {
+      identityMetadata: `${process.env.OAUTH_AUTHORITY}${process.env.OAUTH_ID_METADATA}`,
+      clientID: process.env.OAUTH_APP_ID,
+      responseType: "code id_token",
+      responseMode: "form_post",
+      redirectUrl: process.env.OAUTH_REDIRECT_URI,
+      allowHttpForRedirectUrl: true,
+      clientSecret: process.env.OAUTH_APP_PASSWORD,
+      validateIssuer: false,
+      passReqToCallback: false,
+      scope: process.env.OAUTH_SCOPES.split(" "),
+    },
+    signInComplete
+  )
+);
+
+app.use(
+  session({
+    secret: process.env.OAUTH_APP_PASSWORD,
+    resave: false,
+    saveUninitialized: false,
+    unset: "destroy",
+  })
+);
+require("./routes")(app);
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.error = req.flash("error_msg");
+
+  // Check for simple error string and
+  // convert to layout's expected format
+  let errs = req.flash("error");
+  for (let i in errs) {
+    res.locals.error.push({ message: "An error occurred", debug: errs[i] });
+  }
+
+  next();
+});
+
+app.set("views", path.join(__dirname, "views"));
 
 //Handlebar Views Setup
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.set("views", path.join(__dirname, "/views"));
 
 // Sets up the Express app to handle data parsing
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(require("serve-static")(__dirname + "/../../public"));
-app.use(require("cookie-parser")());
-app.use(require("body-parser").urlencoded({ extended: true }));
-// app.use(
-//   require("express-session")({
-//     secret: "keyboard cat",
-//     resave: true,
-//     saveUninitialized: true,
-//   })
-// );
-app.use(
-  sessions({
-    cookie: { maxAge: 86400000 },
-    store: new MemoryStore({
-      checkPeriod: 86400000, // prune expired entries every 24h
-    }),
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+app.use(cookieParser());
 
 app.use(passport.initialize());
 app.use(passport.session());
-require("./routes")(app);
 
 // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   const err = new Error("Not Found");
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function (req, res, next) {
+  // Set the authenticated user in the
+  // template locals
+  if (req.user) {
+    res.locals.user = req.user.profile;
+  }
+  next();
+});
+app.use("/auth", authRouter);
+app.use("/admin", admin);
 
-// app.use(function (err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render("error", {
-//     message: err.message,
-//     error: app.get("env") === "development" ? err : {},
-//   });
-// });
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next();
+});
+
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  console.log(err.message, err.status);
+});
 
 module.exports = app;
